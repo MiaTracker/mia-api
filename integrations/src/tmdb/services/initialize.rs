165a21ddd95415eb1348774936515798575f4b66
NRA1 <NRA1adamt@gmail.com>
2026-04-17 20:56:19 +0200

    Fixing migration

--- integrations/src/tmdb/services/initialize.rs
+++ integrations/src/tmdb/services/initialize.rs
@@ -2,6 +2,9 @@ use crate::constants;
 use crate::tmdb::services::configuration;
 
 pub async fn initialize() {
+    if constants::TMDB_CONFIGURATION.get().is_some() {
+        return;
+    }
     let config = configuration::details().await.expect("Failed to initialize TMDB_CONFIGURATION");
-    constants::TMDB_CONFIGURATION.set(config).expect("Failed to set TMDB_CONFIGURATION");
+    constants::TMDB_CONFIGURATION.set(config).ok();
 }
